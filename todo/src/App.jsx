4ac16ee643f6f5ca79b8a33cb63f9bb6ccb51c63
bhara <bharathJ222000@gmail.com>
2020-11-22 16:07:42 +0530

    added

---
+++ todo/src/App.jsx
@@ -0,0 +1,23 @@
+// import React, { Component } from 'react';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Head</h1>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
+
+const App = () => {
+  return(
+    <div className="App">
+      <h1>Food</h1>
+    </div>
+  )
+}
+
+export default App;
