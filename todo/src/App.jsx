0fac98782d3d5d19e67aa0ceba0483c658037c0c
bhara <bharathJ222000@gmail.com>
2020-11-22 16:12:14 +0530

    added

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,23 +1,29 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>Head</h1>
-//       </div>
-//     )
-//   }
-// }
-
-// export default App;
-
-const App = () => {
-  return(
-    <div className="App">
-      <h1>Food</h1>
-    </div>
-  )
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'eat'},
+      {id: 2, content: 'sleep'}
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Head</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
+
+// const App = () => {
+//   return(
+//     <div className="App">
+//       <h1>Food</h1>
+//     </div>
+//   )
+// }
+
+// export default App;
