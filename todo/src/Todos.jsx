57cba7459d34087691b0422dbb532c1bbee4de36
bhara <bharathJ222000@gmail.com>
2020-11-22 18:03:57 +0530

    added

--- todo/src/Todos.jsx
+++ todo/src/Todos.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
-const Todos = () => {
+const Todos = ({todos}) => {
+
+    const todoList = todos.length ? (
+        todos.map(todo => {
+            return (
+                <div className="collection-item" key={todo.id}>
+                    <span>{todo.content}</span>
+                </div>
+            )
+        })
+    ) : (
+        <p className="center">You have no todo's left</p>
+    );
     return (
         <div className="todos collection">
             <h1>HELLO</h1>
